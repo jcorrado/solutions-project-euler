c8fa07af68af41ebfbf2ac18716b8f48cc0ed62a
Jereme Corrado <jereme@birchbox.com>
2017-06-09 20:17:35 -0400

    Problem 3 definition

--- src/project_euler/core.clj
+++ src/project_euler/core.clj
@@ -53,3 +53,11 @@
 
 (problem-2 4000000)
 ;; => 4613732
+
+
+;; Problem 3
+
+;; The prime factors of 13195 are 5, 7, 13 and 29.
+;;
+;; What is the largest prime factor of the number 600851475143 ?
+

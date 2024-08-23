86ebfcea0ba58da2604af0240c61a023da4e30d7
Jereme Corrado <jereme@zoion.net>
2024-08-23 12:26:02 -0400

    Add solution to problem 4

--- python/problems.py
+++ python/problems.py
@@ -42,6 +42,7 @@ def problem_2(n):
 # Largest Prime Factor
 # https://projecteuler.net/problem=3
 
+
 def problem_3(n):
     def is_prime(n):
         if n <= 1:
@@ -63,7 +64,55 @@ def problem_3(n):
             if is_prime(quotient):
                 return quotient
 
+
 # print(problem_3(13195))
 # # 29
-print(problem_3(600851475143))
-# 6857
+# print(problem_3(600851475143))
+# # 6857
+
+
+# Problem 4
+# Largest Palindrome Product
+# https://projecteuler.net/problem=4
+
+
+def problem_4():
+    # Generate palindromic numbers that are the product of two
+    # three-digit numbers.
+    # 100*100 -> 999*999 = 10,000 -> 998,001
+    #
+    # This produces 1800 palindromic numbers in the required range.
+    def gen_palindromes():
+        fives, sixes = [], []
+        for i in range(1, 10):
+            for j in range(10):
+                for k in range(10):
+                    for accum, compositor in (
+                        (fives, lambda i, j, k: [i, j, k, j, i]),
+                        (sixes, lambda i, j, k: [i, j, k, k, j, i]),
+                    ):
+                        accum.append(
+                            int("".join([str(x) for x in compositor(i, j, k)]))
+                        )
+        return fives + sixes
+
+    def has_three_digit_factors(n):
+        for i in range(100, 1000):
+            if n % i == 0:
+                quotient = n // i
+                if 100 <= quotient <= 999:
+                    return (i, quotient)
+        return False
+
+    # Iterator from max palindrome looking for one that has two
+    # three-digit factors.
+    palindromes = gen_palindromes()
+    for i in range(len(palindromes) - 1, -1, -1):
+        n = palindromes[i]
+        factors = has_three_digit_factors(n)
+        if factors:
+            return (n, factors)
+
+
+print(problem_4())
+# (906609, (913, 993))

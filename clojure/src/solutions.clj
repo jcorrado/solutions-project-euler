9993723dcf5a122f45282024ba9c7b3681e61d77
Jereme Corrado <jereme@zoion.net>
2024-09-25 15:59:28 -0400

    Add Clj solution for problem #1

--- clojure/src/solutions.clj
+++ clojure/src/solutions.clj
@@ -1,2 +1,14 @@
 (ns solutions)
 
+(defn problem-1 [n]
+  (->> (range 1 n)
+       (filter (fn [n] (or (= 0 (mod n 3))
+                          (= 0 (mod n 5)))))
+       (reduce +)))
+
+(comment
+  (problem-1 10)
+  ;; 23
+  (problem-1 1000)
+  ;; 233168
+  )
